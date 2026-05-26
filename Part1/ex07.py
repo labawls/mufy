e8da13bd9f33ea99ff1082375787bfe7230cad11
labawls <labellekhoo08@gmail.com>
2026-05-26 01:13:20 +0000

    done

--- Part1/ex07.py
+++ Part1/ex07.py
@@ -9,4 +9,9 @@ age = {'Hans': 24, 'Prag': 23, 'Bunyod': 18}
 # 5. Delete key-value pair of 'Bunyod' from the dictionary
 # 6. Print the updated dictionary
 
-# Insert your code
+print(age)
+print(age['Hans'])
+age['Prag'] = 30
+print(age['Prag'])
+del age['Bunyod']
+print(age)      
